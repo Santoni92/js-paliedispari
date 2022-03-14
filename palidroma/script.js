138d04d0f5106f4ce17551b2b0833b8f94a02c8f
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-14 18:03:50 +0100

    nell'esercizio sulla parola palindroma ho aggiunto i due metodi trim() ed tolowerCase() così da tagliare la testa al toro su come venga scritta la parola da parte dell'utente(cioè se inizia a scrivere un po lettere maiuscole e un po minuscole).Comunque ho riscritto la funzione isPal in modo tale da effettuare il controllo su ciascuna lettera che compaone la stringa inserita

--- palidroma/script.js
+++ palidroma/script.js
@@ -5,7 +5,7 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 console.log('JS OK!');  //per verificare che lo script js sia ben collegato all'html
 
 let parolaUtente = prompt("Programma che verifica se la parola inserita dall'utente sia una parola palindroma ovvero una parola che letta o da sinistra verso destra o  viceversa non cambia. Inserisci parola: ");
-
+parolaUtente.trim().toLowerCase();
 isPal(parolaUtente);
 
 /*funzione che data una stringa in ingresso mi stampa la stringa rovesciata,ad esempio, se in ingresso gli
@@ -23,9 +23,37 @@ va a confrontare l'argomento dato in ingresso con la stringa rovesciata*/
 function isPal(parola){
     parolaRovesciata = rovesciaStringa(parola);   /*ottengo come output di questa funzione un array che rappresenta la parola 
                                                   inserita dall'utente scritta da però da destra verso sinistra */
+
+    let isPal = true;
+    for(let i = 0; i < parola.length; i++)
+    {
+        if(parola[i] !== parolaRovesciata[i])
+        {
+            isPal = false;
+        }
+    }
     if(parola === parolaRovesciata){
         return prompt('La parola inserita risulta essere una parola palindroma!!!');
     }else{
-        return prompt('La parola inserita non risulta essere una parola palindroma!!!');;
+        return prompt('La parola inserita non risulta essere una parola palindroma!!!');
     }
 }
+
+/*Oppure senza trasformare la parola inserita dall'utente tutta in minuscolo modifico la mia funzione 'isPal'
+in modo tale che venga effettuato il controllo su ciascuna delle lettere della parola inserita e della sua versione rovesciata
+function isPal(parola){
+    parolaRovesciata = rovesciaStringa(parola)
+    let isPal = true;
+    for(let i = 0; i < parola.length; i++)
+    {
+        if(parola[i] !== parolaRovesciata[i])
+        {
+            isPal = false;
+        }
+    }
+    if(isPal){
+        console.log('La parola inserita risulta essere palindroma!!!');
+    }else{
+        console.log('La parola inserita non risulta essere palindroma!!!');
+    }
+*/
